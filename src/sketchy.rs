ae9be1ddc81bfb0e9788bdeca090d0e5d53b5c49
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-12-22 21:45:27 +1000

    fix

--- src/sketchy.rs
+++ src/sketchy.rs
@@ -60,7 +60,7 @@ pub fn run(sketch: &Path, genotype_index: &Path, threads: i32, ranks: usize, sta
     let mash_reader = BufReader::new(stdout);
     let tail_index: usize = sketch_size.to_string().len(); // <tail_index> to reach shared hashes
 
-    let data_file = File::open(genotype_index)?;
+    let data_file = File::open(&genotype_index)?;
     let data_reader = BufReader::new(data_file);
 
     ranked_sum_of_shared_hashes(mash_reader, data_reader, tail_index, index_size, ranks, stability, progress);
