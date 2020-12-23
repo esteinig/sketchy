ff926ebfacbb495463de6faf32454ac2e87d05df
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-12-23 13:59:03 +1000

    fix

--- src/sketchy.rs
+++ src/sketchy.rs
@@ -123,7 +123,7 @@ pub fn screen(fastx: String, sketch: String, genotypes: String, threads: i32, li
     let mut table = Table::new();
     
     if !pretty {
-        let raw = prettytable::format::TableFormat::new("\t");
+        let raw =  format::FormatBuilder::new().column_separator('\t').build();
         table.set_format(raw);
     }
 
