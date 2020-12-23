aa91e8ea1686502c5c43c4fef6d11c370c61667b
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-12-23 16:12:30 +1000

    fix

--- src/sketchy.rs
+++ src/sketchy.rs
@@ -356,17 +356,14 @@ pub fn get_sketch_files(db: String, sketchy_path: &String)  -> (String, String, 
     let db_name = db_path.file_name().unwrap().to_str().unwrap();
 
     println!("{:?} {:?}", db_path, sketchy_path);
-
-    if !db_path.exists(){
-        // Try using the sketchy_path environment variable with the database name given:
-        let db_path = Path::new(&sketchy_path).join(db_name);
-        println!("{:?}", db_path);
-        if !db_path.exists(){
-            clap::Error::with_description("Database sketch directory is not available", clap::ErrorKind::InvalidValue).exit();
-        }
+    
+    let db_path = if !db_path.exists() {
+        Path::new(&sketchy_path).join(db_name)
+    } else {
+        clap::Error::with_description("Database sketch directory is not available", clap::ErrorKind::InvalidValue).exit();
     };
 
-    
+    println!("{:?}", db_path);
 
     let db_sketch = db_path.join(
         format!("{}.msh", db_name)
