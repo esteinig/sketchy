37c1502607191d444a1c0e45fd702751328bea00
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-12-23 15:58:00 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use clap::{Arg, App, SubCommand};
 
 fn main() -> Result<(), Error> {
 
-    let sketchy_home: String = os::homedir().join(".sketchy").to_str().unwrap();
+    let sketchy_home: String = env::home_dir().unwrap_or("");
     let sketchy_path: String = env::var("SKETCHY_PATH").unwrap_or(sketchy_home);
 
     let matches = App::new("sketchy")
