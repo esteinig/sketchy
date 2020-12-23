7dfd0dc386ca75c01e0c1c9c560ed335a61d8bcc
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-12-23 11:18:34 +1000

    fix

--- src/sketchy.rs
+++ src/sketchy.rs
@@ -284,7 +284,7 @@ fn ranked_sum_of_shared_hashes<R: BufRead>(reader: R, data_reader: BufReader<Fil
                     for (key, feature_map) in sssh.iter_mut() {
                         let feature_value = feature_row[*key] as u32;
                         // Add ssh score to feature value in feature map, or init with 0
-                        let feature_value_sssh = feature_map.entry(feature_value as usize).or_insert(0);
+                        let feature_value_sssh = feature_map.entry(feature_value).or_insert(0);
                         *feature_value_sssh += *ssh;
                     }
 
