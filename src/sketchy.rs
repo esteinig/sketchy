cc045a904e811289b6c65998d448a1c54a069b74
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-12-25 12:06:34 +1000

    fix

--- src/sketchy.rs
+++ src/sketchy.rs
@@ -209,8 +209,8 @@ pub fn predict(ssh: String, mode: String, genotype_index: String, genotype_key: 
         let feature_value = &content[3];
 
         let feature_data = &feature_translation[feature_key];
-        let feature_name = feature_data["name"];
-        let feature_prediction = feature_data["values"][feature_value];
+        let feature_name = &feature_data["name"];
+        let feature_prediction = &feature_data["values"][feature_value];
 
         println!("{:?} {:?} {:?}", read, feature_name, feature_prediction);
     }
