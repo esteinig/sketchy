404b2341f26ab505f49316a930775086eb8208d4
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-12-26 13:08:51 +1000

    fix

--- src/sketchy.rs
+++ src/sketchy.rs
@@ -194,7 +194,7 @@ pub fn predict(ssh: String, mode: String, genotype_index: String, genotype_key: 
     let feature_translation: HashMap<String, Value> = serde_json::from_reader(reader)?;
     
     let current_read: usize = 0;
-    let previous_read: &usize = 0;
+    let previous_read: &usize = &0;
 
     let stdin = std::io::stdin();
     let stdin_reader = BufReader::new(stdin);
