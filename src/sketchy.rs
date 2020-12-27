2dc9c0aa439dfd09f892e7342859b4d057cb6f98
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-12-27 12:19:41 +1000

    fix

--- src/sketchy.rs
+++ src/sketchy.rs
@@ -211,7 +211,7 @@ pub fn predict(ssh: String, mode: String, genotype_index: String, genotype_key: 
             _read_tracker[0] = read.to_string();
             
             let _values: Vec<Vec<String>> = read_prediction.values().cloned().collect();
-            let _lengths: Vec<i32> = _values.iter().map(|x| x.len()).collect();
+            let _lengths: Vec<usize> = _values.iter().map(|x| x.len()).collect();
 
 
 
