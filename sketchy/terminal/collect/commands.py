aae1ee0804591547dc648aedf6cfae92f54956f4
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-02 14:26:45 +1000

    6e6bcdc4aeb5

--- sketchy/terminal/collect/commands.py
+++ sketchy/terminal/collect/commands.py
@@ -25,7 +25,10 @@ def collect(
 
             database_data = []
             for db_path in database_paths:
-                db_header = pandas.read_csv(db_path / 'header.txt', sep="\t", header=None).iloc[0].tolist()
+                db_header = pandas.read_csv(
+                    db_path / 'header.txt', sep="\t", header=None, index=False
+                ).iloc[0].tolist()
+
                 read_limit_paths = db_path.glob("*/")
 
                 read_limit_data = []
@@ -37,7 +40,7 @@ def collect(
                         try:
                             df = pandas.read_csv(file, sep="\t", header=None)
                             df.index = [file.name.strip(".tsv") for _ in df.iterrows()]
-                            print(df)
+
                             if path.name == "stream":
                                 df.columns = ["read"] + db_header
                             elif path.name == "dist":
