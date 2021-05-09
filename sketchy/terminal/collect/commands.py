eff2e3a04fcbf8249547f8fbef9ac1f7a07da329
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-05-10 09:13:43 +1000

    Revert "v0.5.0"
    This reverts commit 4532d518689021333d182d8e679e276f92c2352e.

--- sketchy/terminal/collect/commands.py
+++ sketchy/terminal/collect/commands.py
@@ -46,11 +46,7 @@ def collect(
 
                         try:
                             df = pandas.read_csv(file, sep="\t", header=None)
-                            name = file.name.strip(".tsv").split("_")
-                            fname, replicate = name[0], name[1]
-
-                            df.index = [fname for _ in df.iterrows()]
-                            df['replicate'] = [replicate for _ in df.iterrows()]
+                            df.index = [file.name.strip(".tsv") for _ in df.iterrows()]
 
                             if path.name == "stream":
                                 df.columns = ["read"] + db_header
