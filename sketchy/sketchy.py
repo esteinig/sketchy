bb6d2d4a48995dd873078ce3fcc56575e0199973
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-05-14 11:27:01 +1000

    v0.5.0

--- sketchy/sketchy.py
+++ sketchy/sketchy.py
@@ -765,7 +765,7 @@ class SketchyDiagnostics(PoreLogger):
         for method, mdata in data.groupby("method"):
             for db, ddata in data.groupby("db"):
                 for read_limit, rdata in data.groupby("read_limit"):
-                    accuracy = accuracy_score(rdata['reference'], rdata['call'], average='micro')
+                    accuracy = accuracy_score(rdata['reference'], rdata['call'])
                     precision = precision_score(rdata['reference'], rdata['call'], average='micro')
                     recall = precision_score(rdata['reference'], rdata['call'], average='micro')
 
