ac5aed6820b41ff9f66ace22918197e5ad1b3959
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-05-22 11:40:04 +1000

    v0.5.0

--- sketchy/sketchy.py
+++ sketchy/sketchy.py
@@ -155,9 +155,7 @@ class SketchyDiagnostics(PoreLogger):
                     rpf, sep='\t', header=0,
                     names=['read', 'genotype', 'prediction', 'rank', 'sssh', 'limit', 'pscore']
                 )
-                last_read = pd['read'].max()
-                print(last_read)
-                prediction = pd.loc[pd['read'] == last_read, pd['rank'] == 0]
+                prediction = pd.loc[pd['read'] == pd['read'].max() & pd['rank'] == 0, :]
                 genotype_columns = prediction['genotype'].tolist()
                 pscore_data.append(prediction['pscore'].tolist())
                 samples.append(rpf.name.replace(".raw.tsv", ""))
