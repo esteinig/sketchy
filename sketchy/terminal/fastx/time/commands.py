fa9bbe561a9b428245cf045d5b0f495c120bed09
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-05-23 12:05:17 +1000

    v0.5.0

--- sketchy/terminal/fastx/time/commands.py
+++ sketchy/terminal/fastx/time/commands.py
@@ -72,8 +72,11 @@ def time(fastq, read, index, prefix, delta):
 
 def compute_time_delta(fx, read: int = 100, delta: str = None):
 
-    read_time = fx.loc[fx['read'] == read, 'start_time']
-    print(read_time)
+    read_time = fx[fx.index == read]
+    read_time = read_time.at['start_time']
+    print(fx)
+
+
     if delta == 'first':
         first_read = fx.iloc[0]
         readd = dp.parse(read_time) - \
